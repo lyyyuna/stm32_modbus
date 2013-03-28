f316ce0cd6e3a8017dbef264cc0972a9e9fb0cf4
lyyyuna <lyyyuna@gmail.com>
2013-03-28 21:41:04 +0800

    add test code, pay attention to the address PLC 1, pc 0

--- USER/main.c
+++ USER/main.c
@@ -52,16 +52,60 @@ eMBRegInputCB( UCHAR * pucRegBuffer, USHORT usAddress, USHORT usNRegs )
     return eStatus;
 }
 
+/* add test */
+/* holding register address */
+#define REG_HOLDING_START 0x0000
+/* number of regs */
+#define REG_HOLDING_NREGS 8
+/* content of the holding regs 
+   the test data is from xukai's port */
+uint16_t usRegHoldingBuf[REG_HOLDING_NREGS] = {0x147b,0x3f8e,
+0x147b,0x400e,0x1eb8,0x4055,0x147b,0x408e};
 eMBErrorCode
 eMBRegHoldingCB( UCHAR * pucRegBuffer, USHORT usAddress, USHORT usNRegs, eMBRegisterMode eMode )
 {
-	eMBErrorCode    eStatus = MB_ENOERR;
-	static uint8_t flag = 0;
-	
-	flag == 0 ? GPIO_SetBits(GPIOC,GPIO_Pin_2) : GPIO_ResetBits(GPIOC,GPIO_Pin_2);  
-	flag ^= 1;
+    /* error state */
+    eMBErrorCode    eStatus = MB_ENOERR;
+    /* offset */
+    int16_t iRegIndex;
+    
+    /* test if the reg is in the range */
+    if (((int16_t)usAddress-1 >= REG_HOLDING_START) 
+        && (usAddress-1 + usNRegs <= REG_HOLDING_START + REG_HOLDING_NREGS))
+    {
+        /* compute the reg's offset */
+        iRegIndex = (int16_t)(usAddress-1 - REG_HOLDING_START);
+        switch (eMode)
+        {
+            case MB_REG_READ:
+                while (usNRegs > 0)
+                {
+                    *pucRegBuffer++ = (uint8_t)( usRegHoldingBuf[iRegIndex] >> 8 );
+                    *pucRegBuffer++ = (uint8_t)( usRegHoldingBuf[iRegIndex] & 0xff);
+                    iRegIndex ++;
+                    usNRegs --;
+                }
+                break;
+            case MB_REG_WRITE:
+                while (usNRegs > 0)
+                {
+                    usRegHoldingBuf[iRegIndex] = *pucRegBuffer++ << 8;
+                    usRegHoldingBuf[iRegIndex] |= *pucRegBuffer++;
+                    iRegIndex ++;
+                    usNRegs --;
+                }
+                break;
+                
+        }
+    }
+    else{
+        eStatus = MB_ENOREG;
+    }
+    
     return eStatus;
+    
 }
+
 
 eMBErrorCode
 eMBRegCoilsCB( UCHAR * pucRegBuffer, USHORT usAddress, USHORT usNCoils, eMBRegisterMode eMode )
